804c6b974f1e41278de0dc5e7671a8f74f501b66
Daniel Segal <dlemel8@gmail.com>
2021-12-12 00:05:16 +0200

    consumer: add metrics and run in compose

--- consumer/main.py
+++ consumer/main.py
@@ -1,9 +1,11 @@
 import logging
 import random
 import signal
+import socket
 from struct import unpack
 from threading import Event
 
+from prometheus_client import start_http_server
 from redis import from_url
 from vyper import v as config
 
@@ -20,6 +22,7 @@ def main() -> None:
     LOGGER.info('start to prepare workers')
     random.seed()
     config.automatic_env()
+    start_http_server(config.get_int('metrics_port'))
 
     done = Event()
     for signal_ in (signal.SIGINT, signal.SIGTERM):
@@ -42,7 +45,7 @@ def consume_tasks(done: Event, selector: ProcessorSelector) -> None:
     redis_url = config.get_string('redis_url')
     client = from_url(redis_url)
 
-    processing_tasks_queue_name = config.get_string('processing_tasks_queue_name')
+    processing_tasks_queue_name = '_'.join([config.get_string('processing_tasks_queue_name'), socket.gethostname()])
     published_tasks_queue_name = config.get_string('published_tasks_queue_name')
     LOGGER.info('start to consume tasks')
     while not done.is_set():
--- consumer/processor.py
+++ consumer/processor.py
@@ -4,11 +4,17 @@ from dataclasses import dataclass
 from functools import reduce
 from typing import Sequence
 
+from prometheus_client import Histogram
+from prometheus_client.utils import INF
 from time import time
 
 from protos.task_pb2 import Metadata
 
 LOGGER = logging.getLogger(__file__)
+
+HISTOGRAM_BUCKETS = [10 ** x for x in range(5)] + [INF]
+PROCESSOR_DURATION_HISTOGRAM = Histogram('processor_duration_ms', '', buckets=HISTOGRAM_BUCKETS)
+SELECTOR_DURATION_HISTOGRAM = Histogram('selector_duration_ms', '', buckets=HISTOGRAM_BUCKETS)
 
 
 @dataclass
@@ -30,6 +36,8 @@ class Processor:
         time_so_far = time_ms() - start_time
         simulate_cpu_bound_work(self._min_duration_ms - time_so_far, self._max_duration_ms - time_so_far)
 
+        PROCESSOR_DURATION_HISTOGRAM.observe(time_ms() - start_time)
+
 
 class ProcessorSelector:
     def __init__(self, processors: Sequence[Processor], min_duration_ms: int, max_duration_ms: int):
@@ -45,6 +53,8 @@ class ProcessorSelector:
 
         time_so_far = time_ms() - start_time
         simulate_cpu_bound_work(self._min_duration_ms - time_so_far, self._max_duration_ms - time_so_far)
+
+        SELECTOR_DURATION_HISTOGRAM.observe(time_ms() - start_time)
         return self._processors[processor_id]
 
 
